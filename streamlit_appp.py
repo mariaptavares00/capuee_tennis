d21a9789d36c6a48358e142f1a6d29bf53f199fe
mariaptavares00 <mariaptavares00@gmail.com>
2025-01-28 00:02:41 +0100

    Add files via upload

--- streamlit_appp.py
+++ streamlit_appp.py
@@ -57,7 +57,7 @@ def get_tennis_data():
     tennis_df['age'] = pd.to_numeric(tennis_df['age'])
 
     return tennis_df
-
+tenis_df = get_tennis_data()
 
 
 # -----------------------------------------------------------------------------
